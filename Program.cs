6329a7a018b594bf7fa7d0cd76b39f79e593bd11
shadrackkiprotich <mylesshadrack99@gmail.com>
2019-02-04 14:55:00 +0300

    multiples of fifty

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Multiples_of_fifty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter two numbers");
+            int maxnumber=int.Parse(Console.ReadLine());
+            int minnumber=int.Parse(Console.ReadLine());
+            for (int i = maxnumber; i>minnumber; i--)
+            {
+                if ((i%50)==0)
+                {
+               Console.WriteLine(i);
+                   
+                }
+             //break;
+            }
+        }
+    }
+}
